627dd71dcc19be6ade351c3e79a9ea9a6c4811a2
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-24 21:35:27 -0300

    Implementação dos construtores da classe Oculos

--- src/br/com/fiapride/model/Oculos.java
+++ src/br/com/fiapride/model/Oculos.java
@@ -5,7 +5,16 @@ public class Oculos {
 	public String lente;
 	public int preço;
 	public Boolean antireflexo;
+
 	
+	public Oculos() {
 	}
 
 
+	public Oculos(String lente, int preço, Boolean antireflexo) {
+		this.lente = lente;
+		this.preço = preço;
+		this.antireflexo = antireflexo;
+	}
+
+}
